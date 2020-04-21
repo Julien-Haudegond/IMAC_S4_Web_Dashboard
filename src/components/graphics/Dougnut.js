972d092b6360618909648d6bc625a431ca1690bf
oceane <rioce.oceane@gmail.com>
2020-04-21 20:42:42 +0200

    Add two doughnuts in domain content

--- src/components/graphics/Dougnut.js
+++ src/components/graphics/Dougnut.js
@@ -1,45 +1,83 @@
 import { h } from 'hyperapp'
 import Chart from 'chart.js'
 
-export default ({ labelDoughnut, title, colorOfBackground, datas, widthDoughnut, heightDoughnut }) =>
-  <div class="doughnut">
-    <canvas id="chartDoughnut"
-      oncreate = {(element) => {
-        const ctx = element.getContext('2d')
-        const c = new Chart(ctx, {
-          type: 'doughnut',
+const initChartJs = (element, props) => {
+  const ctx = element.getContext('2d')
+  const chart = new Chart(ctx, {
+    type: 'doughnut',
 
-          data: {
-            labels: labelDoughnut,
-            datasets: [{
-              label: title || 'default title',
-              backgroundColor: colorOfBackground,
-              borderColor: colorOfBackground,
-              data: datas
-            }]
-          },
+    data: {
+      labels: props.labelDoughnut,
+      datasets: [{
+        label: props.title || 'default title',
+        backgroundColor: props.colorOfBackground,
+        borderColor: props.colorOfBackground,
+        data: props.datas
+      }]
+    },
 
-          options: {
-            width: widthDoughnut,
-            height: heightDoughnut,
-            style: 'background-color: #eff1ed',
-            cutoutPercentage: 60,
-            responsive: true,
-            animation: {
-              animateRotate: true
-            }
-            /* layout: {
-              padding: {
-                left: 50,
-                right: 0,
-                top: 0,
-                bottom: 0
-              }
-            } */
-          }
-        })
-        return (c)
-      }}
+    options: {
+      width: props.widthDoughnut,
+      height: props.heightDoughnut,
+      style: 'background-color: #eff1ed',
+      cutoutPercentage: 60,
+      responsive: true,
+      maintainAspectRatio: false,
+      title: {
+        display: true,
+        text: props.titleChartDougnut,
+        position: 'top',
+        fontSize: 35,
+        defaultFontFamily: 'Roboto',
+        fontColor: 'black',
+        fontStyle: 'bold'
+      },
+      legend: {
+        display: true,
+        position: 'bottom',
+        labels: {
+          defaultFontFamily: 'Roboto',
+          fontSize: 20,
+          fontColor: 'black',
+          boxWidth: 30
+        }
+      },
+      animation: {
+        animateRotate: true
+      }
+    }
+  })
+}
+
+const updateChartJS = (element, data) => {
+  if (data.labels.length === 0) {
+    return
+  }
+
+  const idToGet = element.parentElement.id.split('_div')[0] + '_canvas'
+
+  Chart.helpers.each(Chart.instances, (instance) => {
+    if (idToGet === instance.chart.canvas.id) {
+      const upchart = instance.chart
+      upchart.data = data
+      upchart.update()
+    }
+  })
+}
+
+export default ({ labelDoughnut, title, colorOfBackground, datas, widthDoughnut, heightDoughnut, titleChartDougnut}) =>
+  <div id={`${title}_div`} class="doughnut">
+    <canvas id={`${title}_canvas`}
+      oncreate={(element) => initChartJs(element, { labelDoughnut, title, titleChartDougnut, colorOfBackground, datas, widthDoughnut, heightDoughnut })}
+      onupdate={(element) => updateChartJS(element, {
+        labels: labelDoughnut,
+        datasets: [{
+          label: title || 'default title',
+          backgroundColor: colorOfBackground,
+          borderColor: colorOfBackground,
+          data: datas
+        }]
+      })}
       width='300'
       height='300'
     ></canvas>
--- src/components/mainContent/DomainContent.js
+++ src/components/mainContent/DomainContent.js
@@ -20,6 +20,27 @@ export default ({ title, festivals }) => {
         <p> {nboffestiv} festival{pluriel} de ce domaine sur {totalnboffestiv} festivals tous domaines confondus. </p>
       </div>
       <div class="domain-view">
+        <Dougnut
+          title = {'Titre1'}
+          titleChartDougnut = {'Répartition en fonction de la parité des départements'}
+          labelDoughnut = {['Départements pairs', 'Département impairs']}
+          datas = {[festivals.filter(item => (item.departmentNb % 2 === 0)).length, festivals.filter(item => (item.departmentNb % 2 === 1)).length]}
+          colorOfBackground = {['#f1c40f', '#ca1551']}
+          widthDoughnut = {250}
+          heightDoughnut = {250}
+        ></Dougnut>
+        <Dougnut
+          title = {'Titre2'}
+          titleChartDougnut = {'Pics d affluence de ces festivals en fonction des saisons'}
+          labelDoughnut = {['printemps', 'été', 'automne', 'hiver']}
+          datas = {[festivals.filter(item => (item.startMonth === '03 (mars)' || item.startMonth === '04 (avril)' || item.startMonth === '05 (mai)')).length,
+                    festivals.filter(item => (item.startMonth === '06 (juin)' || item.startMonth === '07 (juillet)' || item.startMonth === '08 (août)')).length,
+                    festivals.filter(item => (item.startMonth === '09 (septembre)' || item.startMonth === '10 (octobre)' || item.startMonth === '11 (novembre)')).length,
+                    festivals.filter(item => (item.startMonth === '12 (décembre)' || item.startMonth === '01 (janvier)' || item.startMonth === '02 (février)')).length]}
+          colorOfBackground = {['#f1c40f', '#ca1551', '#94d6cE', '#19a09c']}
+          widthDoughnut = {250}
+          heightDoughnut = {250}
+        ></Dougnut>
         <h1>Domain view: {title}</h1>
         {
           festivals.map(item => <p>{item.name}</p>)
