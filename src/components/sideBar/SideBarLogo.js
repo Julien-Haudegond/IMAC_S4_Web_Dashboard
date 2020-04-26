13cd3b37f72a89e8021aa126172430ebdf26ed69
Unknown <veysset.sarah@gmail.com>
2020-04-26 21:18:22 +0200

    Style correction
    Little update of styles

--- src/components/sideBar/SideBarLogo.js
+++ src/components/sideBar/SideBarLogo.js
@@ -7,7 +7,7 @@ import Logo from '/images/splash.png'
 export default () => {
   return (
     <div class="logo">
-      <Link to='/IMAC_S4_Web_Dashboard/'>
+      <Link to='/IMAC_S4_Web_Dashboard/' class="under-logo">
         <a href="#" class="icon"><h1>Festiv</h1><img src={Logo} alt="accueil"/></a>
       </Link>
     </div>
